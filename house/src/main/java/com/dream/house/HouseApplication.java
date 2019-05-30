9c5619be64f0fc2c2625aef9129eb95638d80296
15927002590@163.com <15927002590@163.com>
2019-05-30 09:20:31 +0800

    Commit.......

--- house/src/main/java/com/dream/house/HouseApplication.java
+++ house/src/main/java/com/dream/house/HouseApplication.java
@@ -1,9 +1,13 @@
 package com.dream.house;
 
+import com.dream.house.config.EnableHttpClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableHttpClient
+@EnableAsync
 public class HouseApplication {
 
     public static void main(String[] args) {
